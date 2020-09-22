fe8f45f467393099b92bfe4ab5dfa43d19c6c2f9
Asad ali <46523348+AsadAli00@users.noreply.github.com>
2020-09-22 14:39:19 +0500

    react class no 2

---
+++ public/precache-manifest.2660b2ba523248fb0264e0acd85335eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85b083ade46af5893b1cdd035ade8cf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab4b6c0efc63f1d5bb50",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ec43b20420103c39c175",
+    "url": "/static/css/main.ca45b562.chunk.css"
+  },
+  {
+    "revision": "ab4b6c0efc63f1d5bb50",
+    "url": "/static/js/2.c76e9c01.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c76e9c01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec43b20420103c39c175",
+    "url": "/static/js/main.832db53f.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
